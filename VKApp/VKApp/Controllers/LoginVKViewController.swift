320a4073dd9bf90a6601f5e18ea0eb0cc98c1272
Temich93s <nil>
2022-11-22 00:45:13 +1000

    Сделал часть ДЗ

--- VKApp/VKApp/Controllers/LoginVKViewController.swift
+++ VKApp/VKApp/Controllers/LoginVKViewController.swift
@@ -69,7 +69,34 @@ extension LoginVKViewController: WKNavigationDelegate {
         let token = params["access_token"]
         guard let safeToken = token else { return }
         Session.instance.token = safeToken
-        print(Session.instance.token)
+        print(params["access_token"])
+        print(params["expires_in"])
+        print(url)
         decisionHandler(.cancel)
+
+        // Конфигурация по умолчанию
+        let configuration = URLSessionConfiguration.default // собственная сессия
+        let session = URLSession(configuration: configuration)
+        // создаем url из строки
+        let url1 =
+            URL(
+                string: "https://api.vk.com/method/friends.get?user_id=759366146&fields=bdate&access_token=" +
+                    safeToken +
+                    "&v=5.131"
+            )
+        // задача для запуска
+        guard let safeURL = url1 else { return }
+        let task = session.dataTask(with: safeURL) { data, _, _ in
+            // в замыкании данные, полученные от сервера, мы преобразуем в json
+            guard let safeData = data else { return }
+            let json = try? JSONSerialization.jsonObject(
+                with: safeData,
+                options: JSONSerialization.ReadingOptions.allowFragments
+            )
+            // выводим в консоль
+            print(json)
+        }
+        // запускаем задачу
+        task.resume()
     }
 }
