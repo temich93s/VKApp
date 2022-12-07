9b6145cfd033708e7dd16c0bcff93c6f36b2fed4
Artem Solovev <88254863+temich93s@users.noreply.github.com>
2022-12-07 13:56:17 +1000

    Update NewsViewController.swift

--- VKApp/VKApp/Controllers/NewsViewController.swift
+++ VKApp/VKApp/Controllers/NewsViewController.swift
@@ -13,7 +13,7 @@ final class NewsViewController: UIViewController {
         static let headerNewsTableViewCellID = "HeaderNewsTableViewCell"
         static let postNewsTableViewCellID = "PostNewsTableViewCell"
         static let photoNewsTableViewCellID = "PhotoNewsTableViewCell"
-        static let countCell = 3
+        static let countCellNumber = 3
     }
 
     // MARK: - Private Outlets
@@ -82,7 +82,7 @@ extension NewsViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        Constants.countCell
+        Constants.countCellNumber
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
