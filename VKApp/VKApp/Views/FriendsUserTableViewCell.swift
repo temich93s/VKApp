2297bb10973e8083a7df6eb5160f0d6c50af0e05
Temich93s <nil>
2022-12-13 19:55:35 +1000

    no message

--- VKApp/VKApp/Views/FriendsUserTableViewCell.swift
+++ VKApp/VKApp/Views/FriendsUserTableViewCell.swift
@@ -5,12 +5,6 @@ import UIKit
 
 /// Ячейка с именем и фотографией друга
 final class FriendsUserTableViewCell: UITableViewCell {
-    // MARK: - Constants
-
-    private enum Constants {
-        static let emptyText = ""
-    }
-
     // MARK: - Private Outlets
 
     @IBOutlet private var friendNameLabel: UILabel!
